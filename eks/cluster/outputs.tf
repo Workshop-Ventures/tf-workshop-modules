d03d4a54210a4ac85cb5dc8ceb6b3df3b3c96609
Jason Carter <jason-workshop@Jasons-MBP.lan>
2023-04-10 11:48:37 -0400

    fix: add output

--- eks/cluster/outputs.tf
+++ eks/cluster/outputs.tf
@@ -22,3 +22,8 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = var.cluster_name
 }
+
+output "cluster_certificate_authority_data" {
+  description = "Cluster Cert Auth Data"
+  value       = module.eks.cluster_certificate_authority_data
+}
