37a47564001a919b8bfdad853fcc912d343ccdec
Jason Carter <jason@workshop.dev>
2023-04-11 16:55:51 -0400

    chore: remove s on output

--- mongo/outputs.tf
+++ mongo/outputs.tf
@@ -1,4 +1,4 @@
 # Use terraform output to display connection strings.
-output "connection_strings" {
+output "connection_string" {
   value = mongodbatlas_cluster.cluster.connection_strings.0.standard_srv
 }
