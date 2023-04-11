3c6ac2abbffa937b0fbcfa953aef5bd1ad4ba259
Jason Carter <jason@workshop.dev>
2023-04-11 16:13:40 -0400

    fix: cluster name in outputs

--- mongo/outputs.tf
+++ mongo/outputs.tf
@@ -1,4 +1,4 @@
 # Use terraform output to display connection strings.
 output "connection_strings" {
-  value = mongodbatlas_cluster.main.connection_strings.0.standard_srv
+  value = mongodbatlas_cluster.cluster.connection_strings.0.standard_srv
 }
